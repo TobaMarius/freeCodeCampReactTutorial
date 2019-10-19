89770f5b3240003682458db3657051c834d54442
mariusToba <mariustoba@protonmail.com>
2019-10-19 20:16:37 +0300

    Completed application part 5

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 
 function TodoItem(props) {
-    console.log(props)
+
     return (
         <div>
-            <input type="checkbox" checked={props.item.completed} />
+            <input type="checkbox" checked={!!props.item.completed} onChange={() => console.log("test")} />
             <span>{props.item.text}</span>
         </div>
     );
