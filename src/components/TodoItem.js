a5a8bb6dbae506145593f54d0b25c829c2b1d80f
mariusToba <mariustoba@protonmail.com>
2019-10-27 19:41:55 +0200

    Phase 7: Added conditional styles.

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -6,7 +6,7 @@ function TodoItem(props) {
     return (
         <div>
             <input type="checkbox" checked={!!props.item.completed} onChange={() => props.handleChange(props.item.id)} />
-            <span>{props.item.text}</span>
+            <span style={props.item.completed ? { textDecoration: "line-through" } : null}>{props.item.text}</span>
         </div>
     );
 }
